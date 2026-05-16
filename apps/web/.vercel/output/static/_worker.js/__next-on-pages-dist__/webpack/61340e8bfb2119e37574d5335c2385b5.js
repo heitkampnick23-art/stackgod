e5e9f907a82d99450fe99ed74a7f14ef41b5a562
nick <israel.anderson.1@icloud.com>
2026-05-16 12:52:55 -0500

    chore: untrack apps/web/.vercel (covered by gitignore now)

--- apps/web/.vercel/output/static/_worker.js/__next-on-pages-dist__/webpack/61340e8bfb2119e37574d5335c2385b5.js
+++
@@ -1,1 +0,0 @@
-var t={},l=(_,r,s)=>(t.__chunk_4475=(o,n,e)=>{Promise.resolve().then(e.bind(e,4357))},t.__chunk_7523=(o,n,e)=>{Promise.resolve().then(e.bind(e,8348))},t);export{l as __getNamedExports};
